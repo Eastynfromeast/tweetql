e2382213d3bdd76541315d23a655ef56b070bb8b
Eastyn <eastynwork@gmail.com>
2024-05-23 10:01:58 +0900

    POST, DELETE, PUT HTTP method are need to be inside of type Mutation in GraphQL world

--- server.js
+++ server.js
@@ -7,18 +7,23 @@ const typeDefs = gql`
 		username: String
 	}
 	type Tweet {
-		id: ID
-		text: String
-		author: User
+		id: ID!
+		text: String!
+		author: User!
 	}
 
 	type Query {
-		allTweets: [Tweet]
-		tweet(id: ID): Tweet
+		allTweets: [Tweet!]!
+		tweet(id: ID!): Tweet!
+	}
+	type Mutation {
+		postTweet(text: String!, userId: ID!): Tweet!
+		deleteTweet(id: ID!): Boolean!
 	}
 `;
 
 // GET /api/v1/tweets
+// POST /api/v1/tweets
 // GET /api/v1/tweet/:id
 
 const server = new ApolloServer({ typeDefs });
@@ -35,4 +40,29 @@ server.listen().then(({ url }) => {
 		allTweets: [Tweet]
 		tweet(id: ID): Tweet // receiving an argument
 	}
+
+    type Mutation == POST request of REST api
+        things are mutataed in the backend such as database
+        the data user sends to the database and mutate the data
+        POST, DELETE, PUT is all Mutation
+
+    This is query by default
+    and if you want to mutate this, you need to write mutation 
+    {
+        allTweets {
+        text
+        }
+        tweet(id: "1") {
+        author {
+            username
+        }
+        text
+        }
+    }
+
+    mutation{
+        postTweet(text:"Hello,first tweet!", userId:"1") {
+        text
+        }
+    }
 */
